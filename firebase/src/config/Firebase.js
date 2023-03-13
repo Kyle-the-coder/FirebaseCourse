53424fc155563842cc22351f4308d53bbe7a8587
Kyle Mitchell <kylethacoder@gmail.com>
2023-03-13 15:17:45 -0700

    updated

--- firebase/src/config/Firebase.js
+++ firebase/src/config/Firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA-50MEc_U_KLFEYvyjdZk-YaxGCO0mfPc",
-  authDomain: "fir-course91.firebaseapp.com",
-  projectId: "fir-course91",
-  storageBucket: "fir-course91.appspot.com",
-  messagingSenderId: "1016658627693",
-  appId: "1:1016658627693:web:a2df8ebfb3dfaed87a4364",
-  measurementId: "G-VV2TNJ56YM"
+    apiKey: "AIzaSyA-50MEc_U_KLFEYvyjdZk-YaxGCO0mfPc",
+    authDomain: "fir-course91.firebaseapp.com",
+    projectId: "fir-course91",
+    storageBucket: "fir-course91.appspot.com",
+    messagingSenderId: "1016658627693",
+    appId: "1:1016658627693:web:a2df8ebfb3dfaed87a4364",
+    measurementId: "G-VV2TNJ56YM"
 };
 
 // Initialize Firebase
